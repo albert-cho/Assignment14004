5abedb2a53af5c3212a1f2db2c430c6fa6f1ae89
albert-cho <albert.cho1332@gmail.com>
2017-09-30 12:57:54 -0400

    User setters

--- eclipse-Neon/4004A1/SampleLibrary/src/server/logic/model/User.java
+++ eclipse-Neon/4004A1/SampleLibrary/src/server/logic/model/User.java
@@ -5,21 +5,38 @@ public class User {
 	static String username = "John";
 	static String password = "Password";
 	
+	public User(int userid,String username, String password){
+		this.userid=userid;
+		this.username=username;
+		this.password=password;
+	}
 	
 	public String toString(){
 		return "["+this.userid+","+this.username+","+this.password+"]";
 	}
-	
+		
 	public static int getUserid() {
 		return userid;
+	}
+	
+	public void setUserid(int userid){
+		this.userid=userid;
 	}
 
 	public static String getUsername() {
 		return username;
 	}
+	
+	public void setUsername(String username){
+		this.username=username;
+	}
 
 	public static String getPassword() {
 		return password;
+	}
+	
+	public void setPassword(String password){
+		this.password=password;
 	}
 
 }
