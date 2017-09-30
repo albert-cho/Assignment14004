d6d79ff7ec50548995a1418137d90008c5aa8a2e
albert-cho <albert.cho1332@gmail.com>
2017-09-30 13:33:11 -0400

    Item tests

---
+++ eclipse-Neon/4004A1/SampleLibrary/src/server/logic/model/Fee.java
@@ -0,0 +1,5 @@
+package server.logic.model;
+
+public class Fee {
+
+}
---
+++ eclipse-Neon/4004A1/SampleLibrary/src/server/logic/model/Tests/FeeTests.java
@@ -0,0 +1,14 @@
+package server.logic.model.Tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FeeTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
