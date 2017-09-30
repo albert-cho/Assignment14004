3f910da886a9511dc587d2b8c7c30885c9f9a699
albert-cho <albert.cho1332@gmail.com>
2017-09-30 13:44:35 -0400

    Fee Tests

--- eclipse-Neon/4004A1/SampleLibrary/src/server/logic/model/Tests/FeeTests.java
+++ eclipse-Neon/4004A1/SampleLibrary/src/server/logic/model/Tests/FeeTests.java
@@ -4,11 +4,18 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import server.logic.model.Fee;
+
 public class FeeTests {
 
+	Fee tFee = new Fee(0, 0);
+	
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
+		assertEquals(0, tFee.getUserid());
+		assertEquals(0, tFee.getFee());
+		assertEquals("[0,0]", tFee.toString());
 	}
 
 }
