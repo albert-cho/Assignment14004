505ee09f280a6dd4ce2efb82508d426e28940199
albert-cho <albert.cho1332@gmail.com>
2017-10-07 15:16:11 -0400

    UserTable tests without delete

--- eclipse-Neon/4004A1/SampleLibrary/src/server/logic/tables/Tests/UserTableTests.java
+++ eclipse-Neon/4004A1/SampleLibrary/src/server/logic/tables/Tests/UserTableTests.java
@@ -4,13 +4,26 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import server.logic.tables.UserTable;
+
 public class UserTableTests {
 
-	//UserTable tUtable = new UserTable();
+	UserTable tUtable = UserTable.getInstance();
 	
 	@Test
 	public void test() {
 		//fail("Not yet implemented");
+		assertEquals(-1, tUtable.lookup("Test"));
+		assertEquals(false, tUtable.lookup(5));
+		assertEquals(true, tUtable.createuser("Test", "Password"));
+		
+		assertEquals(5, tUtable.lookup("Test"));
+		assertEquals(true, tUtable.lookup(5));
+		assertEquals(false, tUtable.createuser("Test", "Password"));
+		
+		assertEquals(0, tUtable.checkUser("Test", "Password"));
+		assertEquals(1, tUtable.checkUser("Test", ""));
+		assertEquals(2, tUtable.checkUser("Tests", "Hello"));
 	}
 
 }
