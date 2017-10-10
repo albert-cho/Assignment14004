173104c02c3a4e979e22cf3d9de38d6ae77e3e59
albert-cho <albert.cho1332@gmail.com>
2017-10-10 07:31:01 -0400

    Setup for add Item acceptance test

--- eclipse-Neon/4004A1/SampleLibrary/src/tests/acceptance/addItem.java
+++ eclipse-Neon/4004A1/SampleLibrary/src/tests/acceptance/addItem.java
@@ -4,12 +4,35 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import server.logic.handler.InputHandler;
+import server.logic.handler.model.ServerOutput;
+
 public class addItem {
 
+	ServerOutput tServerOut = null;
+	InputHandler tInput = new InputHandler();
+	
 	@Test
 	public void test() {
-		//fail("Not yet implemented");
+		//Clerk Requests to add title
+		tServerOut = tInput.processInput("create item", InputHandler.CLERK);
+		assertEquals("Please Input Item Info:'ISBN'", tServerOut.getOutput());
+		assertEquals(InputHandler.CREATEITEM, tServerOut.getState());
 		
+		//Title information is added
+		////The ISBN number does not exist
+		tServerOut = tInput.processInput("1111111111111", InputHandler.CREATEITEM);
+		assertEquals("The Title Does Not Exists!", tServerOut.getOutput());
+		assertEquals(InputHandler.CLERK, tServerOut.getState());
+		//have to get this to instead ask to add a title
+		////see addTitle for adding title
+		
+		////The Item gets added successfully
+		tServerOut = tInput.processInput("9781442668584", InputHandler.CREATEITEM);
+		assertEquals("Success!", tServerOut.getOutput());
+		assertEquals(InputHandler.CLERK, tServerOut.getState());
+		
+		////T
 	}
 
 }
